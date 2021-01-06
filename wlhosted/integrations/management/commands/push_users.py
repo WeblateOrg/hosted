1e88dba04894818b3666de11965fc7979e6019d7
Michal Čihař <michal@cihar.com>
2021-01-06 10:02:48 +0100

    Add management command to push all users

---
+++ wlhosted/integrations/management/commands/push_users.py
@@ -0,0 +1,31 @@
+#
+# Copyright © 2012 - 2020 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from django.core.management.base import BaseCommand
+from webalte.auth.models import User
+
+from wlhosted.integrations.models import propagate_user_changes
+
+
+class Command(BaseCommand):
+    help = "pushes user data to weblate.org"
+
+    def handle(self, *args, **options):
+        for user in User.objects.filter(is_active=True).iterator():
+            propagate_user_changes(None, user)
