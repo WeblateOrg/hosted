0228b2a327b971a98197dc195cf861ce25c2479b
Michal Čihař <michal@cihar.com>
2024-10-25 19:09:18 +0200

    feat: add command to list contacts

---
+++ wlhosted/integrations/management/commands/list_contacts.py
@@ -0,0 +1,44 @@
+#
+# Copyright © Michal Čihař <michal@weblate.org>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from django.core.management.base import BaseCommand
+from weblate.billing.models import Billing
+from weblate.auth.models import User
+
+
+class Command(BaseCommand):
+    help = "lists payments using obsolete payment method"
+
+    def handle(self, *args, **options):
+        emails = set()
+        for billing in (
+            Billing.objects.filter(state=Billing.STATE_ACTIVE)
+            .exclude(plan__price=0)
+            .prefetch()
+        ):
+            for user in billing.owners.all():
+                emails.add(user.email)
+            for project in billing.ordered_projects:
+                for user in User.objects.having_perm("billing.manage", project):
+                    emails.add(user.email)
+
+        emails.discard("")
+
+        for email in sorted(emails):
+            self.stdout.write(email)
